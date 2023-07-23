6ade90dacaa84c8a81cce6b8d87bd891ab5010cc
bobby stearman <bobby@didcoding.com>
2023-07-23 13:41:07 +0100

    fixed black formatting issue in validators

--- app/backend/course/conf/common.py
+++ app/backend/course/conf/common.py
@@ -117,8 +117,7 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation\
-            .NumericPasswordValidator",
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
