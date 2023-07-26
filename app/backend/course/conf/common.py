b2b4df85de129cb0b886cf77d2129053424f752a
bobby-oddersea <bobby@oddersea.com>
2023-07-26 13:35:17 +0100

    removed context-processor

--- app/backend/course/conf/common.py
+++ app/backend/course/conf/common.py
@@ -93,7 +93,6 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
                 # PROJECT CONTEXT
-                "course.context_processor.project_context",
             ],
         },
     },
