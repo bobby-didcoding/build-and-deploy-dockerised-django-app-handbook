dc99a3ad595bfb3d0ab7105f95be7ad88c5826ab
Bobby Stearman <80459294+bobby-didcoding@users.noreply.github.com>
2023-07-31 12:20:19 +0100

    Update stage.py

--- app/backend/course/conf/stage.py
+++ app/backend/course/conf/stage.py
@@ -2,7 +2,7 @@
 # Python  imports
 # --------------------------------------------------------------
 import os
-import socket
+import logging.config
 
 # --------------------------------------------------------------
 # Project imports
@@ -40,7 +40,21 @@ from .common import (
     COOKIE_BOT,
     STRIPE_SECRET_KEY,
     STRIPE_PUBLISHABLE,
+    STRIPE_WEBHOOK_SECRET,
+    STRIPE_WEBHOOK_KEY,
+    CELERY_BROKER_URL,
+    CELERY_RESULT_BACKEND,
+    CELERY_ACCEPT_CONTENT,
+    CELERY_RESULT_SERIALIZER,
+    CELERY_TASK_SERIALIZER,
+    CELERY_BEAT_SCHEDULER,
 )
+
+# --------------------------------------------------------------
+# Django  imports
+# --------------------------------------------------------------
+from django.utils.log import DEFAULT_LOGGING
+from django.core.management.color import supports_color
 
 # --------------------------------------------------------------
 # 3rd Party imports
@@ -83,6 +97,14 @@ RECAPTCHA_PRIVATE_KEY = RECAPTCHA_PRIVATE_KEY
 COOKIE_BOT = COOKIE_BOT
 STRIPE_SECRET_KEY = STRIPE_SECRET_KEY
 STRIPE_PUBLISHABLE = STRIPE_PUBLISHABLE
+STRIPE_WEBHOOK_SECRET = STRIPE_WEBHOOK_SECRET
+STRIPE_WEBHOOK_KEY = STRIPE_WEBHOOK_KEY
+CELERY_BROKER_URL = CELERY_BROKER_URL
+CELERY_RESULT_BACKEND = CELERY_RESULT_BACKEND
+CELERY_ACCEPT_CONTENT = CELERY_ACCEPT_CONTENT
+CELERY_RESULT_SERIALIZER = CELERY_RESULT_SERIALIZER
+CELERY_TASK_SERIALIZER = CELERY_TASK_SERIALIZER
+CELERY_BEAT_SCHEDULER = CELERY_BEAT_SCHEDULER
 
 PRODUCTION = 1
 DEBUG = 1
