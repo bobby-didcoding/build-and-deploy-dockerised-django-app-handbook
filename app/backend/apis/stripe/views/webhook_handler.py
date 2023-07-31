c7e0e52bf1e436f84a64e0f1e68a57afd7238c13
Bobby Stearman <80459294+bobby-didcoding@users.noreply.github.com>
2023-07-31 12:10:15 +0100

    Update webhook_handler.py

--- app/backend/apis/stripe/views/webhook_handler.py
+++ app/backend/apis/stripe/views/webhook_handler.py
@@ -9,6 +9,7 @@ import logging
 # --------------------------------------------------------------
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
 
 
 # --------------------------------------------------------------
@@ -19,33 +20,61 @@ from apis.stripe import (
     InvoiceWebhook,
 )
 
+# --------------------------------------------------------------
+# 3rd party imports
+# --------------------------------------------------------------
+import stripe
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
+
 logger = logging.getLogger(__name__)
 
 
 @csrf_exempt
 def stripe_webhooks(request):
-    # We will protect this view with a VPN
-    try:
-        payload = request.body
-        event = json.loads(payload)
-    except ValueError as e:
-        logging.error(
-            "❌🌐 Failed to process webhook while parsing basic request." + str(e)
-        )
-        return JsonResponse({"success": True}, status=400)
+    
+    if settings.STRIPE_WEBHOOK_KEY:
+        try:
+            sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+            event = None
+            payload = request.body
+            endpoint_secret = settings.STRIPE_WEBHOOK_SECRET
+            try:
+                event = stripe.Webhook.construct_event(
+                payload, sig_header, endpoint_secret
+                )
+            except ValueError as e:
+                # Invalid payload
+                return HttpResponse(status=400)
+            except stripe.error.SignatureVerificationError as e:
+                # Invalid signature
+                return HttpResponse(status=400)
+        except KeyError:
+            return HttpResponse(status=404) 
 
-    event_list = event["type"].split(".")
-    if len(event_list) == 3:
-        del event_list[0]
+    if request.method == "POST":
+        try:
+            payload = request.body
+            event = json.loads(payload)
+        except ValueError as e:
+            logging.error(
+                "❌🌐 Failed to process webhook while parsing basic request." + str(e)
+            )
+            return JsonResponse({"success": True}, status=400)
 
-    match event_list[0]:
-        case "customer":
-            handler = CustomerWebhook(event["data"]["object"], event_list[1])
-        case "invoice":
-            handler = InvoiceWebhook(event["data"]["object"], event_list[1])
-        case _:
-            handler = None
-    if handler:
-        if hasattr(handler, event_list[1]):
-            getattr(handler, event_list[1])()
-    return HttpResponse(status=200)
+        event_list = event["type"].split(".")
+        if len(event_list) == 3:
+            del event_list[0]
+
+        match event_list[0]:
+            case "customer":
+                handler = CustomerWebhook(event["data"]["object"], event_list[1])
+            case "invoice":
+                handler = InvoiceWebhook(event["data"]["object"], event_list[1])
+            case _:
+                handler = None
+        if handler:
+            if hasattr(handler, event_list[1]):
+                getattr(handler, event_list[1])()
+        return HttpResponse(status=200)
+    return HttpResponse(status=404)
